7e001f45d89cdfa35d161bc1934716d4088f4fe2
LanceGuy5 <lanceforresth@gmail.com>
2024-09-09 15:08:49 -0400

    fixed stupid nextjs error and lint

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,6 +19,17 @@ export default function Home() {
       <Head>
         <title>mix-ai</title>
         <link rel='icon' href='/favicon.ico' />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <meta charSet='UTF-8' />
+        <meta name='description' content='mix-ai' />
+        <meta name='mobile-web-app-capable' content='yes' />
+        <meta name='apple-mobile-web-app-capable' content='yes' />
+        <meta
+          name='apple-mobile-web-app-status-bar-style'
+          content='black-translucent'
+        />
+        <meta name='format-detection' content='telephone=no' />
+        <meta name='apple-mobile-web-app-title' content='mix-ai' />
       </Head>
       <header>
         <div className='flex flex-col items-center'>

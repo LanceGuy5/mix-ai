a06bfa9bc718ffa08f484a3661407a399c2816a9
LanceGuy5 <lanceforresth@gmail.com>
2024-09-10 15:11:13 -0400

    writing streams n shit (mp3 path needs to be specified tho but whatever

--- src/pages/api/getPlaylist.tsx
+++ src/pages/api/getPlaylist.tsx
@@ -1,20 +1,30 @@
 import axios from 'axios';
+import scdl from 'soundcloud-downloader';
+import * as fs from 'fs';
 
-const scdl = require('soundcloud-downloader').default;
-const fs = require('fs');
-
-const GetPlaylistWithId = async (req: any, res: any) => {
-  const SOUNDCLOUD_URL = `${process.env.SOUNDCLOUD_URL}`;
+const GetPlaylistWithURL = async (
+  req: any,
+  res: any
+): Promise<{ status: number; message: string }> => {
+  const SOUNDCLOUD_URL = `https://soundcloud.com/lance-hartman-129699056/sets/random/s-gKjL1NhvLie?si=d87af33719894805a2a8c565168d79d1&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing`;
   const CLIENT_ID = `${process.env.SOUNDCLOUD_CLIENT_ID}`;
   scdl
-    .download(SOUNDCLOUD_URL)
-    .then((stream: any) => stream.pipe(fs.createWriteStream('audio.mp3')));
+    .downloadPlaylist(SOUNDCLOUD_URL)
+    .then((stream: any) => stream.pipe(fs.createWriteStream('audio.mp3')))
+    .then(() => {
+      return { status: 200, message: 'success' };
+    })
+    .catch(() => {
+      return { status: 500, message: 'stream_error' };
+    });
+  return { status: 200, message: 'error' };
 };
 
 const handler = async (req: any, res: any): Promise<any> => {
   if (req.method === 'POST') {
-    await GetPlaylistWithId(req, res);
-    res.status(200).send('OK');
+    const result: { status: number; message: string } =
+      await GetPlaylistWithURL(req, res);
+    res.status(result.status).send(result.message);
   } else {
     res.status(405).send('Method Not Allowed');
   }

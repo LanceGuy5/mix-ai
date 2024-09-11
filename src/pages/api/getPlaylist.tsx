b68063ee91c493258d1a93281577f876f6a25c31
LanceGuy5 <lanceforresth@gmail.com>
2024-09-10 23:15:11 -0400

    implementing custom node lib

--- src/pages/api/getPlaylist.tsx
+++ src/pages/api/getPlaylist.tsx
@@ -1,7 +1,6 @@
-import scdl from 'soundcloud-downloader';
+import scdl from 'node-fetch-sd';
 import * as fs from 'fs';
 import { v4 as uuidv4 } from 'uuid';
-import { toNodeReadableStream } from '@/lib/utils';
 
 const GetPlaylistWithURL = async (
   req: any,
@@ -10,13 +9,10 @@ const GetPlaylistWithURL = async (
   const SOUNDCLOUD_URL = `https://soundcloud.com/lance-hartman-129699056/sets/random/s-gKjL1NhvLie?si=d87af33719894805a2a8c565168d79d1&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing`;
   scdl
     .downloadPlaylist(SOUNDCLOUD_URL)
-    .then((stream: [ReadableStream<any>[], String[]]) => {
-      console.log('GOT STREAM, WRITING TO AUDIO FILES');
+    .then((stream: [NodeJS.ReadableStream[], String[]]) => {
       for (let i = 0; i < stream[0].length; i++) {
         console.log('writing to audio file: ' + stream[1][i]);
-        const writeStream = toNodeReadableStream(stream[0][i]); // TODO SO SLOW HERE
-        console.log('writeStream generated');
-        writeStream.pipe(
+        stream[0][i].pipe(
           fs.createWriteStream(`songs/${stream[1][i]}_${uuidv4()}.mp3`)
         );
       }

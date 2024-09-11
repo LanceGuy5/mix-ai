33358b2a59c7d9b822f08340cf9b5acf0143d711
Luka Koll <110080569+lkoll123@users.noreply.github.com>
2024-09-11 09:54:58 -0400

    Update FormBox.tsx
    Edit FormBos; input still lacking styling

--- src/components/ui/FormBox.tsx
+++ src/components/ui/FormBox.tsx
@@ -1,6 +1,9 @@
 import axios from 'axios';
+import React, {useState} from 'React';
 
 const getPlaylist = async (id: number) => {
+
+  
   const playlist = await axios.request({
     method: 'POST',
     url: `/api/getPlaylist`,
@@ -11,16 +14,35 @@ const getPlaylist = async (id: number) => {
 };
 
 export default function FormBox() {
+  const [signedIn, setSignedIn]  = useState(false);
+  const [playlist, setPlaylist] = useState("");
+  const toggleSigned = () => {
+    setSignedIn(curr => !curr);
+  }
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  }
   return (
     <div className='form-box'>
-      <button
-        onClick={() => {
-          getPlaylist(1212781357);
-        }}
-        className='rounded bg-blue-500 px-4 py-2 font-bold text-white transition-colors duration-300 hover:bg-blue-700'
-      >
-        Sign In with SoundCloud
-      </button>
+      <form onSubmit={handleSubmit}>
+        {!signedIn && <button
+          onClick={async () => {
+            await getPlaylist(1212781357);
+            toggleSigned
+          }}
+          className='rounded bg-blue-500 px-4 py-2 font-bold text-white transition-colors duration-300 hover:bg-blue-700'
+        >
+          Sign In with SoundCloud
+        </button>
+        }
+        {signedIn && <div>
+          <input type="text" name="email" id="email" required placeholder="Enter your playlist URL" onChange = {(e: React.ChangeEvent<HTMLInputElement>) => {
+            const value = e.target.value;
+            setPlaylist(value);
+          }}/>
+          <button type="submit" className="bold-button rounded bg-blue-500 px-4 py-2 font-bold text-white transition-colors duration-300 hover:bg-blue-700">Mix</button>
+          </div>}
+      </form>
     </div>
   );
 }

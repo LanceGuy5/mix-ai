de3ffe3705e044b15d5e811ef27fd7ccc5583340
LanceGuy5 <lanceforresth@gmail.com>
2024-09-11 11:17:03 -0400

    bruh luka

--- src/components/ui/FormBox.tsx
+++ src/components/ui/FormBox.tsx
@@ -1,9 +1,7 @@
 import axios from 'axios';
-import React, {useState} from 'React';
+import React, { useState } from 'React';
 
 const getPlaylist = async (id: number) => {
-
-  
   const playlist = await axios.request({
     method: 'POST',
     url: `/api/getPlaylist`,
@@ -14,34 +12,49 @@ const getPlaylist = async (id: number) => {
 };
 
 export default function FormBox() {
-  const [signedIn, setSignedIn]  = useState(false);
-  const [playlist, setPlaylist] = useState("");
+  const [signedIn, setSignedIn] = useState(false);
+  const [playlist, setPlaylist] = useState('');
   const toggleSigned = () => {
-    setSignedIn(curr => !curr);
-  }
+    setSignedIn((curr) => !curr);
+  };
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-  }
+  };
   return (
     <div className='form-box'>
       <form onSubmit={handleSubmit}>
-        {!signedIn && <button
-          onClick={async () => {
-            await getPlaylist(1212781357);
-            toggleSigned
-          }}
-          className='rounded bg-blue-500 px-4 py-2 font-bold text-white transition-colors duration-300 hover:bg-blue-700'
-        >
-          Sign In with SoundCloud
-        </button>
-        }
-        {signedIn && <div>
-          <input type="text" name="email" id="email" required placeholder="Enter your playlist URL" onChange = {(e: React.ChangeEvent<HTMLInputElement>) => {
-            const value = e.target.value;
-            setPlaylist(value);
-          }}/>
-          <button type="submit" className="bold-button rounded bg-blue-500 px-4 py-2 font-bold text-white transition-colors duration-300 hover:bg-blue-700">Mix</button>
-          </div>}
+        {!signedIn && (
+          <button
+            onClick={async () => {
+              await getPlaylist(1212781357);
+              toggleSigned;
+            }}
+            className='rounded bg-blue-500 px-4 py-2 font-bold text-white transition-colors duration-300 hover:bg-blue-700'
+          >
+            Sign In with SoundCloud
+          </button>
+        )}
+        {signedIn && (
+          <div>
+            <input
+              type='text'
+              name='email'
+              id='email'
+              required
+              placeholder='Enter your playlist URL'
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                const value = e.target.value;
+                setPlaylist(value);
+              }}
+            />
+            <button
+              type='submit'
+              className='bold-button rounded bg-blue-500 px-4 py-2 font-bold text-white transition-colors duration-300 hover:bg-blue-700'
+            >
+              Mix
+            </button>
+          </div>
+        )}
       </form>
     </div>
   );
